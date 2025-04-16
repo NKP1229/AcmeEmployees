478435548b2dfd0ec545604ef697bf9c5bbc6ff3
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-16 17:06:56 -0400

    can start and see home page & url to get employees

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 // imports here for express and pg
-
+const express = require("express");
+const app = express();
+const pg = require("pg");
+const PORT = 3000;
+const client = new pg.Client("postgres://localhost/nickeolla");
+//--> const cors = require("cors");
 // static routes here (you only need these for deployment)
 
 // app routes here
+//--> app.use(cors());
+app.listen(PORT, () => {
+  console.log(`I am listening on port number ${PORT}`);
+});
 
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+app.get("/api/employees", (req, res) => {
+  res.send("Get Employees.");
+});
 // create your init function
-
+const init = async (req, res) => {
+  try {
+    await client.connect();
+  } catch (error) {
+    console.error(error);
+  }
+};
 // init function invocation
+init();
